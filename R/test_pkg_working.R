e90f1ff486612082ba6d1a2dc90bad1dcbd8aab8
jwg301 <jwgarrow@willamette.edu>
2025-06-20 15:18:38 -0700

    Initial commit

---
+++ R/test_pkg_working.R
@@ -0,0 +1,6 @@
+usethis::use_git()
+usethis::create_github_token() # create and securely store your PAT
+gitcreds::gitcreds_set()
+usethis::use_github() # publishes to your GitHub account
+usethis::use_roxygen_md() # check DESCRIPTION for roxygen notes
+usethis::use_r("my_function") # create or modify a function
